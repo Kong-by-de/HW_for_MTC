2d2e0b98ed8dcd2e394007707d02a10ec5fc9be3
Kong-by-de <snich195@mail.ru>
2025-10-27 12:55:50 +0300

    Hw_7

---
+++ src/main/java/com/mipt/aleksandrivanovich/Hw_7/CustomList.java
@@ -0,0 +1,15 @@
+package com.mipt.aleksandrivanovich.Hw_7;
+
+
+public interface CustomList<A> {
+
+  void add(A element);
+
+  A get(int index);
+
+  A remove(int index);
+
+  int size();
+
+  boolean isEmpty();
+}
